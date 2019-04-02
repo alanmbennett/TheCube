5705ddcb081e8221e077549e5cf018a0d0b6c429
Alan B <alanmitchellbennett@gmail.com>
2019-04-02 12:12:17 -0400

    Initial Commit

---
+++ TheCube/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TheCube
+//
+//  Created by Alan Bennett on 4/2/19.
+//  Copyright © 2019 Alan Bennett. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
